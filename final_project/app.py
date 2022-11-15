87f14323918f1999121fd0971b84ffa31989c553
Warlord1172 <damiangonzalez604@gmail.com>
2022-11-15 17:27:34 -0600

    made website configuration

--- final_project/app.py
+++ final_project/app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
-app.run(host='0.0.0.0',port=8080,debug=False)
+    return render_template("website.html")
+app.run(host='0.0.0.0',port=8080,debug=True)
